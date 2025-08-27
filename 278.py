3a1a66f1563278c851b5efe7aa1289722c0bae65
rodrii11926 <uriarterodrigo6@gmail.com>
2025-08-27 11:52:38 +0000

    z

---
+++ 278.py
@@ -0,0 +1,21 @@
+import math
+raiz_cuadrada = math.sqrt(144)
+entero = int(raiz_cuadrada)
+raiz_cubica = math.cbrt(entero)
+#podemos hacer raices elvando un numero a un exponente de fraccion 
+igual = raiz_cuadrada == 144 ** (1/2)
+#usar math.pow es lo mismo que usar ""
+numero_al_cuadrado = math.pow(144,2)
+igualar_cuadrados = numero_al_cuadrado == 144 ** 2 
+print (" es lo mismo usar pow que **", igualar_cuadrados)
+print(entero)
+raiz_cuadraticas = 16 ** (1/4)
+igualar_cuadraticas = raiz_cuadratica == math.pow(16,1/4)
+print(raiz_cuadratica)
+print(" las raices cuadraticas son iguales)" ,igualar_cuadraticas)
+*print(igual)
+numero_pimath.pi
+print(numero_pi)
+pi_al_cuadrado = numero_pi ** 2
+print(pi_al_cuadrado)
+print(numero_de_eular)
