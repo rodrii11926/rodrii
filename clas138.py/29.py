1e84ebd7975b641d6b5c2caa6b9116268c80ae89
rodrii11926 <uriarterodrigo6@gmail.com>
2025-10-29 11:55:44 +0000

    pppp

--- clas138.py/29.py
+++ clas138.py/29.py
@@ -1,4 +1,9 @@
-numero1 = 5
-numero2 = 10
-suma = numero1 + numero2
-print("La suma de", numero1, "y", numero2, "es:", suma)
+numero1= 7
+numero2= 23
+suma=  numero1 + numero2
+print (suma)
+if numero1 > numero2:
+    print("numero1> numero2")
+if numero1 < numero2:
+    print("numero1 < numero2")
+print(10 - 5)
